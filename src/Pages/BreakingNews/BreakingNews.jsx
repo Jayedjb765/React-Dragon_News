f41f917ac53dde0b851d65d1adb2fc433e60fb45
Jayedjb765 <jayedjb76@gmail.com>
2024-08-21 20:18:46 +0600

    Updating Navbar

--- src/Pages/BreakingNews/BreakingNews.jsx
+++ src/Pages/BreakingNews/BreakingNews.jsx
@@ -1,9 +1,15 @@
-
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 const BreakingNews = () => {
     return (
-        <div>
+        <div className="flex">
             <button className="btn btn-secondary"> Breaking News </button>
+            <Marquee pauseOnClick={true} speed={100}>
+             <Link className="mr-12">I can be a React component, multiple React components, or just some text......</Link> 
+             <Link className="mr-12">I can be a React component, multiple React components, or just some text......</Link> 
+             <Link className="mr-12">I can be a React component, multiple React components, or just some text......</Link> 
+         </Marquee>
             
         </div>
     );
